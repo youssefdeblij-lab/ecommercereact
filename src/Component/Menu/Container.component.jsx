9da4d310f43498d95cd92adf887c5f963d6e0fc6
youssefdeblij <yousse.deblij@gmail.com>
2021-07-06 10:38:50 +0100

    very first commit

---
+++ src/Component/Menu/Container.component.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../Menu/Container.styles.scss'
+
+export const Container = () => <div className="Homepage">
+    <div className="directory-menu">
+        
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HAT</h1>
+            <span className="subtitle"> Shop now</span>
+            </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1  className="title">Sneakers</h1>
+            <span className="subtitle">Shop now</span>
+            </div>
+        </div>
+        <div className="menu-item">
+         <div className="content">
+            <h1  className="title">Jeans</h1>
+            <span className="subtitle">Shop now</span>
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className="content">
+                <h1  className="title">Mens</h1>
+                <span className="subtitle">Shop now</span>
+            </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1  className="title">Women</h1>
+            <span className="subtitle">Shop now</span>
+         </div>
+        </div>
+    </div>
+
+</div>
